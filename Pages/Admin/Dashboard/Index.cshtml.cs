96f87c06809e2f83081f1c77ba799b1345e09efa
hailuu <hoanghai.luu.71@gmail.com>
2022-10-19 12:14:09 +0700

    doing dashboard

--- Pages/Admin/Dashboard/Index.cshtml.cs
+++ Pages/Admin/Dashboard/Index.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebRazor.Models;
 
 namespace WebRazor.Pages.Admin.Dashboard
 {
     public class IndexModel : PageModel
     {
+        private PRN221DBContext dbContext;
+        public IndexModel(PRN221DBContext context)
+        {
+            dbContext = context;
+        }
+        public List<OrderInMonth> ordersInMonth { get; set; } = new();
         public void OnGet()
         {
+            for(var i = 1; i <= 12; i++)
+            {
+                ordersInMonth.Add(new OrderInMonth() { orders = dbContext.Orders.Where(order => order.OrderDate.Value.Month == i && order.OrderDate.Value.Year == DateTime.Today.Year).ToList()}); 
+            }
+        }
+        public class OrderInMonth
+        {
+            public List<Models.Order> orders = new();
         }
     }
 }
--- Pages/Admin/Product/Index.cshtml.cs
+++ Pages/Admin/Product/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebRazor.Models;
 
@@ -14,9 +15,27 @@ namespace WebRazor.Pages.Product
         }
 
         public List<Models.Product> Products { get; set; }
-        public async Task<IActionResult> OnGet()
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+        public SelectList? Categories { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? Category {get; set;}
+        public async Task<IActionResult> OnGetAsync()
         {
-            Products = dbContext.Products.Include(p => p.Category).ToList();
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                Products = dbContext.Products.Include(p => p.Category).Where(p => p.ProductName.Contains(SearchString)).ToList();
+            }
+            else
+            {
+                Products = dbContext.Products.Include(p => p.Category).ToList();
+            }
+            if (!string.IsNullOrEmpty(Category))
+            {
+                Products = dbContext.Products.Include(p => p.Category).Where(p => p.Category.CategoryName.Equals(Category)).ToList();
+            }
+            Categories = new SelectList(dbContext.Categories.Select(cat => cat.CategoryName).ToList());
+
             return Page();
         }
     }
